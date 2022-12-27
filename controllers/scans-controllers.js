0c909b9e02efbbe9a13af38d92214645323cc596
tasduqali <tasduqali2@gmail.com>
2022-12-27 14:17:40 +0400

    scans

--- controllers/scans-controllers.js
+++ controllers/scans-controllers.js
@@ -40,16 +40,12 @@ const submitScans = async function (body) {
       const { userId, doctorId, doctorName, faceScanImages, teethScanImages } =
         body;
       try {
-        if (
-          userId ||
-          doctorId ||
-          doctorName ||
-          faceScanImages ||
-          teethScanImages
-        ) {
+        if (userId && doctorId) {
           if (
-            (faceScanImages && faceScanImages.length > 0) ||
-            (teethScanImages && teethScanImages.length > 0)
+            faceScanImages ||
+            faceScanImages.length > 0 ||
+            teethScanImages ||
+            teethScanImages.length > 0
           ) {
             let updatedFaceScanImages = [];
             for (i = 0; i < faceScanImages.length; i++) {
